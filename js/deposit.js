bd9afa65cc14fb9e2293bd7619e228cb1fc42c86
sultan-cse <nishtan49@gmail.com>
2024-08-26 00:31:29 +0600

    baap er bank completed

--- js/deposit.js
+++ js/deposit.js
@@ -1,21 +1,20 @@
 document.getElementById('deposit-btn').addEventListener('click',function(){
    const depositField = document.getElementById('deposit-field');
    const depositAmount = parseFloat(depositField.value);
-  
-   if((isNaN(depositField.value)) || depositField.value === '' || depositAmount < 0){
-    depositField.value = '';
-    alert("Please give a valid amount");
+   depositField.value = '';
+   if((isNaN(depositAmount))){
+    alert("give valid amount");
+    return;
     
    }
   
    else{
    const depositText = document.getElementById('deposit-text');
    const depositPreviousValue = parseFloat(depositText.innerText);
-   depositText.innerText = depositAmount + depositPreviousValue;
    const balanceText = document.getElementById('balance-text');
    const previousBalance = parseFloat(balanceText.innerText);
+   depositText.innerText = depositAmount + depositPreviousValue;
    balanceText.innerText = previousBalance + depositAmount;
-   depositField.value = '';
    }
   
 
--- js/withdraw.js
+++ js/withdraw.js
@@ -1,22 +1,24 @@
 document.getElementById('withdraw-btn').addEventListener('click',function(){
     const withdrawField = document.getElementById('withdraw-field');
     const withdrawAmount = parseFloat(withdrawField.value);
+    console.log(isNaN(withdrawAmount));
+    withdrawField.value = '';
+    if((isNaN(withdrawAmount))){
+        alert("Please give a valid amount");
+        return;
+        
+       }
     const balanceText = document.getElementById('balance-text');
     const previousBalance = parseFloat(balanceText.innerText);
-   
-    if((isNaN(withdrawField.value)) || withdrawField.value === '' || withdrawAmount > previousBalance ){
-     withdrawField.value = '';
-     alert("Please give a valid amount");
-     
-    }
-   
-    else{
+    
     const withdrawText = document.getElementById('withdraw-text');
     const withdrawPreviousValue = parseFloat(withdrawText.innerText);
+    if(withdrawAmount>previousBalance){
+        alert("amount is too high");
+        return;
+    }
     withdrawText.innerText = withdrawAmount + withdrawPreviousValue;
     balanceText.innerText = previousBalance - withdrawAmount;
-    withdrawField.value = '';
-    }
    
  
  })
